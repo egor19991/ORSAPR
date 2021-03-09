2320a007a3a07579076dd359f65b3d65d9670f3b
egorkrasnov1999 <egorkrasnov1999@yandex.ru>
2021-03-09 19:04:02 +0700

    Добавлены словарь для текстбоксов(практика 09.03.2021)

--- LampPlugin/LampPlugingGUI/MainForm.Designer.cs
+++ LampPlugin/LampPlugingGUI/MainForm.Designer.cs
@@ -115,7 +115,8 @@ namespace LampPluginUI
             this.textBox3.Location = new System.Drawing.Point(141, 66);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(45, 20);
-            this.textBox3.TabIndex = 7;
+            this.textBox3.TabIndex = 7;            
+            this.textBox3.Leave += new System.EventHandler(this.DiametrBodyTextBox_Leave);
             // 
             // textBox4
             // 
--- LampPlugin/LampPlugingGUI/MainForm.cs
+++ LampPlugin/LampPlugingGUI/MainForm.cs
@@ -16,6 +16,9 @@ namespace LampPluginUI
 {
     public partial class MainForm : Form
     {
+
+        private readonly Dictionary<TextBox, Action<Lamp, string>> _textBoxDictionary;
+
         public MainForm()
         {
             InitializeComponent();
@@ -32,6 +35,17 @@ namespace LampPluginUI
                 MessageBox.Show(e.Message);
                 //throw;
             }
+            _textBoxDictionary = new Dictionary<TextBox, Action<Lamp, string>>()
+            {
+                {
+                    DiametrBodyTextBox, 
+                    (Lamp lamp, string text) => {lamp.Body.Diametr = Int32.Parse(text);}
+                },
+                {
+                    textBox3, 
+                    (Lamp lamp, string text) => {lamp.Body.Height = Int32.Parse(text);}
+                }
+            }; 
         }
 
         private Lamp _lamp = new Lamp{};
@@ -46,43 +60,46 @@ namespace LampPluginUI
         /// <param name="e"></param>
         private void DiametrBodyTextBox_Leave(object sender, EventArgs e)
         {
+            var currentTextBox = (TextBox)sender;
+            var currentAction = _textBoxDictionary[currentTextBox];
+
             //Svin(DiametrBodyTextBox,  _lamp.Body.Diametr);
-            if (DiametrBodyTextBox.Text != String.Empty)
+            if (currentTextBox.Text != String.Empty)
             {
                 try
                 {
-                    _lamp.Body.Diametr = Int32.Parse(DiametrBodyTextBox.Text);
-                    DiametrBodyTextBox.BackColor = Color.White;
+                    currentAction.Invoke(_lamp, currentTextBox.Text);
+                    currentTextBox.BackColor = Color.White;
                 }
                 catch (ArgumentException exception) //по идее должно быть ArgumentException, но Exception- защита от букв и точек!
                 {
-                    DiametrBodyTextBox.BackColor = Color.LightCoral;
+                    currentTextBox.BackColor = Color.LightCoral;
                     MessageBox.Show(exception.Message);
                 }
             }
         }
 
         //!!!!!!! Tessstt !!!!!! Спросить 2
-        //public void Svin(TextBox svin1, int svin2)
-        //{
-        //    if (svin1.Text != String.Empty)
-        //    {
-        //        try
-        //        {
-        //            svin2 = Int32.Parse(svin1.Text);
-        //            svin1.BackColor = Color.White;
-        //            MessageBox.Show(svin2.ToString());
-        //        }
-        //        catch (ArgumentException exception) //по идее должно быть ArgumentException, но Exception- защита от букв и точек!
-        //        {
+        public void Svin(TextBox svin1, int svin2)
+        {
+            if (svin1.Text != String.Empty)
+            {
+                try
+                {
+                    svin2 = Int32.Parse(svin1.Text);
+                    svin1.BackColor = Color.White;
+                    MessageBox.Show(svin2.ToString());
+                }
+                catch (ArgumentException exception) //по идее должно быть ArgumentException, но Exception- защита от букв и точек!
+                {
 
-        //            // Console.WriteLine(exception.Message);
-        //            //throw;
-        //            svin1.BackColor = Color.LightCoral;
-        //            MessageBox.Show(exception.Message);
-        //        }
-        //    }
-        //}
+                    // Console.WriteLine(exception.Message);
+                    //throw;
+                    svin1.BackColor = Color.LightCoral;
+                    MessageBox.Show(exception.Message);
+                }
+            }
+        }
 
 
 
